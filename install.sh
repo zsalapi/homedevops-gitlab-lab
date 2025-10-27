7ec4eaa1b2fa2a560e00dfff14cfbb236937e6da
Zsolt Alapi <zsalapi@gmail.com>
2025-10-27 17:06:20 +0100

    telepítő fix

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ mkdir -p ./netdata/netdata_cache
 mkdir -p ./netdata/netdata_config
 mkdir -p ./netdata/netdata_lib
 mkdir -p ./backup
+mkdir -p ./grafana_data
+mkdir -p ./runner/config
 cp ./config.toml ./runner/config/
 echo "Most már futtathatod: docker-compose up -d"
